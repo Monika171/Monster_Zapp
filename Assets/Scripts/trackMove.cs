e9210e2221aaf691da8270057c038b32917f73af
MonikaR17 <monikarabha171@gmail.com>
2020-07-18 05:57:08 +0900

    InitialRelease-OldProject2018

---
+++ Assets/Scripts/trackMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class trackMove : MonoBehaviour
+{
+    public float speed;
+    Vector2 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector2(0, Time.time * speed);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+        
+    }
+}
